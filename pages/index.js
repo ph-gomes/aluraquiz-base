17e8e7996f4b077f14f29a6ecbb67ca94dd1d054
ph-gomes <radago@hotmail.com.br>
2021-01-26 12:06:13 -0300

    Day 01

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+import db from "../db.json";
+import QuizBackground from "../src/components/QuizBackground";
+import Widget from "../src/components/Widget";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+export default function Home() {
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Css</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="github.com/ph-gomes" />
+    </QuizBackground>
+  );
+}
